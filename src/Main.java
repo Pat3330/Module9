ccea4e1b12937dbe340447a3467a7195b8cec08c
patco <patcortez163@gmail.com>
2026-04-19 23:11:05 -0400

    Commit Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number of customers: ");
+        int numCustomers = scanner.nextInt();
+
+        System.out.print("Number of cashiers: ");
+        int numCashiers = scanner.nextInt();
+
+        System.out.print("Queue capacity: ");
+        int capacity = scanner.nextInt();
+
+        CheckoutQueue queue = new CheckoutQueue(capacity);
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        for (int i = 1; i <= numCustomers; i++) {
+            exec.execute(new Customer(i, queue));
+        }
+
+        for (int i = 1; i <= numCashiers; i++) {
+            exec.execute(new Cashier(i, queue));
+        }
+
+        exec.shutdown();
 
     }
 }
